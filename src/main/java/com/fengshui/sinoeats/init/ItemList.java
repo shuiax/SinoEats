b03c8c4d0f2ab24806d8f65ef564cf18ef8a0fcb
shuiax <0annieliu0@gmail.com>
2020-08-20 21:28:35 -0700

    foods

--- src/main/java/com/fengshui/sinoeats/init/ItemList.java
+++ src/main/java/com/fengshui/sinoeats/init/ItemList.java
@@ -2,6 +2,7 @@ package com.fengshui.sinoeats.init;
 
 import com.fengshui.sinoeats.Main;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,5 +18,10 @@ public class ItemList {
     public static final RegistryObject<Item> RICE_SEEDS = ITEMS.register("rice_crop",
             () -> new BlockItem(BlockList.RICE_CROP.get(),
                     new Item.Properties().group(Main.TAB)));
+
+    public static final RegistryObject<Item> RICE = ITEMS.register("rice",
+            () -> new Item(new Item.Properties().group(Main.TAB)
+                    .food(new Food.Builder().hunger(6).saturation(1.2f).build())));
+
 
 }
